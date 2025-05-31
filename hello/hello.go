633ce5c7e2a5a906e829f20f90199d2eeab11d02
kemal <kemaltaufikf@gmail.com>
2025-06-01 01:58:16 +0700

    how to handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
     // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    message, _ := greetings.Hello("Gladys")
     fmt.Println(message)
+
+    // Request a greeting
+    messageError, err2 := greetings.Hello("")
+    fmt.Println(messageError, err2)
+
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err2 != nil {
+        log.Fatal(err2)
+    }
 }
